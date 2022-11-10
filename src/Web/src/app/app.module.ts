f1dc5c0137e8f9826944da3b6e04852d56d958e5
Felipe Gallego <felipegallego600@gmail.com>
2022-11-09 22:57:55 -0500

    Primer comit

--- src/Web/src/app/app.module.ts
+++ src/Web/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+
+
+const routes: Routes = [
+  { path: 'Prueba', component: LoginRegisterComponent },
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash:true}),
+
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
