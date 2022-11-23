93ca45159eb6f87451c0f8f091c84a9ea8672ce2
TheChec <chechoosorio@hotmail.com>
2022-11-22 19:17:00 -0500

    Program

--- src/Web/src/app/components/navigator/navigator.component.ts
+++ src/Web/src/app/components/navigator/navigator.component.ts
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigator.component.css']
 })
 export class NavigatorComponent implements OnInit {
-
+   contador:number = 1;
   constructor() { }
 
   ngOnInit(): void {
     
-  
+  }
+  Hidden(){
+      
+    
+    if(this.contador==0){
+        document.querySelector("#Central")?.classList.add("visto"),
+        this.contador=1}
+
+    else{
+        document.querySelector("#Central")?.classList.remove("visto"),
+        this.contador=0
+    }
+
+
 
   }
-
 }
