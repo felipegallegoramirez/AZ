ea417199226ce18a49992b9c50be8f4501a2e7a1
Felipe Gallego <felipegallego600@gmail.com>
2024-03-09 23:01:10 -0500

    simple-shop-function

--- src/Web/src/app/components/simple-shop-main/simple-shop-main.component.ts
+++ src/Web/src/app/components/simple-shop-main/simple-shop-main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Inventory } from 'src/app/models/modelsProducts/inventory';
+import { InventoryService } from 'src/app/services/servicesProducts/inventory.service';
+
 
 @Component({
   selector: 'app-simple-shop-main',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimpleShopMainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private inventoryService:InventoryService,
+
+  ) { }
 
   ngOnInit(): void {
+    this.getProducts()
   }
 
+  inventorys:Array<Inventory>=[]
+  cart:Array<Inventory>=[]
+
+  getProducts(){
+    this.inventoryService.getInventorys().subscribe((res) =>{
+
+      this.inventorys = res as Inventory[]
+    })
+  }
+
+  add(i:number){
+    let index=this.cart.findIndex(x=>x._id==this.inventorys[i]._id)
+    if(index!=-1){
+        this.cart[index].count=(this.cart[index].count||0)+1 ;
+
+    }else{
+      this.cart.push(this.inventorys[i])
+      this.cart[this.cart.length-1].count=1
+    }
+
+  }
+
+  next(){
+    localStorage.setItem("cart",JSON.stringify(this.cart))
+    window.location.replace("http://localhost:4200/#/shop-cart");
+  }
+
+//    let x=JSON.parse(localStorage.getItem("cart")||"")
 }
